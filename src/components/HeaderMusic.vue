beb2c7ce870d6903aba18a4fdf27a968f82dc7fc
flowersunshine <31510360+flowersunshine@users.noreply.github.com>
2018-06-12 19:12:29 +0800

    11

--- src/components/HeaderMusic.vue
+++ src/components/HeaderMusic.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <span><Icon type="headphone"></Icon>伪网易云音乐</span>
+    <span><Icon type="headphone"></Icon>音乐播放器</span>
     <Input type="text" v-model="search" placeholder="搜索音乐，视频，歌词电台" style="width:250px" icon="search" size="large" @on-enter="doSearch()"/>
   </div>
 </template>
